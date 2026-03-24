6b3c8c423a0841596814c05448cd35d4b0f3519d
lam7039 <kelvin7039@gmail.com>
2026-03-24 19:10:16 +0100

    added selectable characters for trim trait, use mb_ methods in format trait

--- src/Support/StrTraits/StrFormatTrait.php
+++ src/Support/StrTraits/StrFormatTrait.php
@@ -5,25 +5,25 @@ namespace Stella\Support\StrTraits;
 trait StrFormatTrait {
     public function camel(): self {
         $value = preg_replace('/[\s\-_]+/', ' ', $this->value);
-        $value = ucwords($value);
+        $value = mb_convert_case($value, MB_CASE_TITLE);
         $value = str_replace(' ', '', $value);
-        return new self(lcfirst($value));
+        return new self(mb_lcfirst($value));
     }
 
     public function pascal(): self {
-        return new self(ucfirst($this->camel()->value()));
+        return new self(mb_ucfirst($this->camel()->value()));
     }
 
     public function snake(): self {
         $value = preg_replace('/[A-Z]/', '_$0', $this->value);
-        $value = strtolower(trim($value, '_'));
+        $value = mb_strtolower(trim($value, '_'));
         $value = str_replace(' ', '_', $value);
         return new self($value);
     }
 
     public function slug(): self {
         $value = preg_replace('/[^A-Za-z0-9]+/', '-', $this->value);
-        $value = strtolower(trim($value, '-'));
+        $value = mb_strtolower(trim($value, '-'));
         return new self($value);
     }
 }
--- src/Support/StrTraits/StrTrimTrait.php
+++ src/Support/StrTraits/StrTrimTrait.php
@@ -3,19 +3,19 @@
 namespace Stella\Support\StrTraits;
 
 trait StrTrimTrait {
-    public function trim(): self {
-        return new self(trim($this->value));
+    public function trim(string $characters = " \n\r\t\v\x00"): self {
+        return new self(trim($this->value, $characters));
     }
 
-    public function ltrim(): self {
-        return new self(ltrim($this->value));
+    public function ltrim(string $characters = " \n\r\t\v\x00"): self {
+        return new self(ltrim($this->value, $characters));
     }
 
-    public function rtrim(): self {
-        return new self(rtrim($this->value));
+    public function rtrim(string $characters = " \n\r\t\v\x00"): self {
+        return new self(rtrim($this->value, $characters));
     }
 
-    public function squish(): self {
-        return new self(str_replace(' ', '', $this->value));
+    public function squish(string|array $characters = [" ", "\n", "\r", "\t", "\v", "\0"]): self {
+        return new self(str_replace($characters, '', $this->value));
     }
 }
