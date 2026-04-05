ed02bf257ea11804cb70bd0ebd029dbe95f1097e
lam7039 <kelvin7039@gmail.com>
2026-04-05 02:19:59 +0200

    refactor EscapeTrait; add entityDecode method and remove redundant entityEncode method for improved string handling

--- src/Support/Traits/Str/EscapeTrait.php
+++ src/Support/Traits/Str/EscapeTrait.php
@@ -15,26 +15,18 @@ trait EscapeTrait {
         return $this->with(htmlspecialchars_decode($this->value(), $flags));
     }
 
+    public function entityDecode(
+        int $flags = ENT_QUOTES | ENT_SUBSTITUTE,
+        ?string $encoding = 'UTF-8'
+    ) {
+        return $this->with(html_entity_decode($this->value(), $flags, $encoding));
+    }
+
     public function urlEncode(): self {
         return $this->with(rawurlencode($this->value()));
     }
 
     public function urlDecode(): self {
         return $this->with(rawurldecode($this->value()));
-    }
-
-    public function entityEncode(
-        int $flags = ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML401,
-        ?string $encoding = 'UTF-8',
-        bool $doubleEncode = true
-    ): self {
-        return $this->with(htmlentities($this->value(), $flags, $encoding, $doubleEncode));
-    }
-
-    public function entityDecode(
-        int $flags = ENT_QUOTES | ENT_SUBSTITUTE,
-        ?string $encoding = 'UTF-8'
-    ) {
-        return $this->with(html_entity_decode($this->value(), $flags, $encoding));
     }
 }
