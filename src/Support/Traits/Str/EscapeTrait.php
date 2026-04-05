9d918e9b7e217ad6d52a6d0635043596e834a7b6
lam7039 <kelvin7039@gmail.com>
2026-04-05 02:09:32 +0200

    refactor EscapeTrait; add methods for URL and entity encoding/decoding to enhance string manipulation capabilities

--- src/Support/Traits/Str/EscapeTrait.php
+++ src/Support/Traits/Str/EscapeTrait.php
@@ -10,4 +10,31 @@ trait EscapeTrait {
     ): self {
         return $this->with(htmlspecialchars($this->value(), $flags, $encoding, $doubleEncode));
     }
+
+    public function htmlDecode(int $flags = ENT_QUOTES | ENT_SUBSTITUTE): self {
+        return $this->with(htmlspecialchars_decode($this->value(), $flags));
+    }
+
+    public function urlEncode(): self {
+        return $this->with(rawurlencode($this->value()));
+    }
+
+    public function urlDecode(): self {
+        return $this->with(rawurldecode($this->value()));
+    }
+
+    public function entityEncode(
+        int $flags = ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML401,
+        ?string $encoding = 'UTF-8',
+        bool $doubleEncode = true
+    ): self {
+        return $this->with(htmlentities($this->value(), $flags, $encoding, $doubleEncode));
+    }
+
+    public function entityDecode(
+        int $flags = ENT_QUOTES | ENT_SUBSTITUTE,
+        ?string $encoding = 'UTF-8'
+    ) {
+        return $this->with(html_entity_decode($this->value(), $flags, $encoding));
+    }
 }
