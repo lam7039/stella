909b413a22608330e9c1ce7dcb9c32f15592b206
lam7039 <kelvin7039@gmail.com>
2026-07-09 20:28:41 +0200

    refactor CastTrait methods; improve error handling and ensure type safety

--- src/Support/Traits/Str/CastTrait.php
+++ src/Support/Traits/Str/CastTrait.php
@@ -18,17 +18,23 @@ trait CastTrait
 
     public function bool(): bool
     {
-        return boolval($this->value());
+        return filter_var($this->value(), FILTER_VALIDATE_BOOLEAN);
     }
 
     public function array(bool $associative = false): array
     {
-        return json_decode($this->value(), $associative);
+        $value = json_decode($this->value(), $associative);
+
+        if (! is_array($value)) {
+            throw new \Exception; //TODO: JsonDecodeException;
+        }
+
+        return $value;
     }
 
     public function json(): string
     {
-        return json_encode($this->value());
+        return json_encode($this->value(), JSON_THROW_ON_ERROR);
     }
 
     public function toBase64(): string
@@ -38,7 +44,9 @@ trait CastTrait
 
     public function fromBase64(bool $strict = true): string
     {
-        if (! $decoded = base64_decode($this->value(), $strict)) {
+        $decoded = base64_decode($this->value(), $strict);
+
+        if ($decoded === false) {
             throw new \Exception; //TODO: Base64DecodeException;
         }
 
