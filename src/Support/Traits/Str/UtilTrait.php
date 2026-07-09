53e8fd8e263ae3b46c8bba885b90754ee2b83f25
lam7039 <kelvin7039@gmail.com>
2026-07-09 20:29:27 +0200

    add uuid generation method to UtilTrait; improve string utility functionality

--- src/Support/Traits/Str/UtilTrait.php
+++ src/Support/Traits/Str/UtilTrait.php
@@ -24,4 +24,15 @@ trait UtilTrait
 
         return $this->with($this->value());
     }
+
+    //TODO: separate uuid generation to a separate class, this is not a string utility
+    public function uuid(): string
+    {
+        $data = random_bytes(16);
+
+        $data[6] = chr((ord($data[6]) & 0x0f) | 0x40);
+        $data[8] = chr((ord($data[8]) & 0x3f) | 0x80);
+
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
 }
