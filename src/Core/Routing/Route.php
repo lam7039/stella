7fcb5209d4335462a562fa5afc2957a2786c97b5
lam7039 <kelvin7039@gmail.com>
2026-07-18 18:59:20 +0200

    feat(Routing): implement Route and Router classes for HTTP request handling

--- src/Core/Routing/Route.php
+++ src/Core/Routing/Route.php
@@ -1,3 +1,43 @@
 <?php
 
 namespace Stella\Core\Routing;
+
+use Stella\Core\Http\Request\Request;
+use Stella\Core\Http\Request\RequestMethod;
+use Stella\Core\Http\Response\Response;
+
+class Route
+{
+    public function __construct(
+        public readonly RequestMethod $method,
+        public readonly string $uri,
+        public readonly mixed $handler,
+        protected array $middleware = []
+    ) {
+    }
+
+    public function middleware(string|array $middleware): static
+    {
+        if (is_string($middleware)) {
+            $middleware = [$middleware];
+        }
+
+        $this->middleware = array_merge($this->middleware, $middleware);
+
+        return $this;
+    }
+
+    public function run(Request $request): Response
+    {
+        if (is_callable($this->handler)) {
+            return call_user_func($this->handler, $request);
+        }
+
+        [$controller, $method] = $this->handler;
+
+        return new $controller()->{$method}($request);
+    }
+
+    //TODO: middleware
+    //TODO: groups like web, api
+}
--- src/Core/Routing/Router.php
+++ src/Core/Routing/Router.php
@@ -1,3 +1,54 @@
 <?php
 
 namespace Stella\Core\Routing;
+
+use Stella\Core\Http\Request\Request;
+use Stella\Core\Http\Request\RequestMethod;
+
+class Router
+{
+    protected array $routes = [];
+
+    public function get(string $uri, mixed $handler): Route
+    {
+        return $this->addRoute(RequestMethod::GET, $uri, $handler);
+    }
+
+    public function post(string $uri, mixed $handler): Route
+    {
+        return $this->addRoute(RequestMethod::POST, $uri, $handler);
+    }
+
+    public function put(string $uri, mixed $handler): Route
+    {
+        return $this->addRoute(RequestMethod::PUT, $uri, $handler);
+    }
+
+    public function delete(string $uri, mixed $handler): Route
+    {
+        return $this->addRoute(RequestMethod::DELETE, $uri, $handler);
+    }
+
+    protected function addRoute(RequestMethod $method, string $uri, mixed $handler): Route
+    {
+        $route = new Route($method, $uri, $handler);
+
+        $this->routes[] = $route;
+
+        return $route;
+    }
+
+    public function match(Request $request): Route
+    {
+        foreach ($this->routes as $route) {
+            if (
+                $route->method === $request->method() &&
+                $route->uri === $request->uri()
+            ) {
+                return $route;
+            }
+        }
+
+        throw new \RuntimeException('No matching route found for ' . $request->method() . ' ' . $request->uri());
+    }
+}
